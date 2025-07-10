8a980b30cf667f22cb7a08c2370262819234071b
Tidiane-junior <77800865+Tidiane-junior@users.noreply.github.com>
2025-07-10 22:19:33 +0200

    Add files via upload

---
+++ data/secteur_dactivite_3.js
@@ -0,0 +1,20 @@
+var json_secteur_dactivite_3 = {
+"type": "FeatureCollection",
+"name": "secteur_dactivite_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "nom": "Ile-de-France", "secteur": "Commerce", "Population": "12450849" }, "geometry": { "type": "Point", "coordinates": [ 2.503362114387945, 48.709325442282342 ] } },
+{ "type": "Feature", "properties": { "nom": "Centre-Val de Loire", "secteur": "Industrie", "Population": "2581479" }, "geometry": { "type": "Point", "coordinates": [ 1.684336741688517, 47.48465147615768 ] } },
+{ "type": "Feature", "properties": { "nom": "Bourgogne-Franche-Comte", "secteur": "Agricole", "Population": "2793080" }, "geometry": { "type": "Point", "coordinates": [ 4.80675170497518, 47.234196888935543 ] } },
+{ "type": "Feature", "properties": { "nom": "Normandie", "secteur": "Agricole", "Population": "3341312" }, "geometry": { "type": "Point", "coordinates": [ 0.109821603864672, 49.120146662691162 ] } },
+{ "type": "Feature", "properties": { "nom": "Hauts-de-France", "secteur": "Telecom", "Population": "5973933" }, "geometry": { "type": "Point", "coordinates": [ 2.771763286323771, 49.969382230776382 ] } },
+{ "type": "Feature", "properties": { "nom": "Grand Est", "secteur": "Agricole", "Population": "5544051" }, "geometry": { "type": "Point", "coordinates": [ 5.612981161819823, 48.688730186922271 ] } },
+{ "type": "Feature", "properties": { "nom": "Pays de la Loire", "secteur": "Industrie", "Population": "3936719" }, "geometry": { "type": "Point", "coordinates": [ -0.816747738053424, 47.478253780516482 ] } },
+{ "type": "Feature", "properties": { "nom": "Bretagne", "secteur": "Industrie", "Population": "3475895" }, "geometry": { "type": "Point", "coordinates": [ -2.84182542921611, 48.177840537383531 ] } },
+{ "type": "Feature", "properties": { "nom": "Nouvelle-Aquitaine", "secteur": "Telecom", "Population": "6191209" }, "geometry": { "type": "Point", "coordinates": [ 0.212097995964508, 45.205793827520175 ] } },
+{ "type": "Feature", "properties": { "nom": "Occitanie", "secteur": "Industrie", "Population": "6201587" }, "geometry": { "type": "Point", "coordinates": [ 2.14427443601543, 43.702814344926075 ] } },
+{ "type": "Feature", "properties": { "nom": "Auvergne-Rhone-Alpes", "secteur": "Telecom", "Population": "8260096" }, "geometry": { "type": "Point", "coordinates": [ 4.536463167493149, 45.514714332010911 ] } },
+{ "type": "Feature", "properties": { "nom": "Provence-Alpes-Cote d'Azur", "secteur": "Tourisme", "Population": "5241587" }, "geometry": { "type": "Point", "coordinates": [ 6.060323260941492, 43.958742421884502 ] } },
+{ "type": "Feature", "properties": { "nom": "Corse", "secteur": "Tourisme", "Population": "360162" }, "geometry": { "type": "Point", "coordinates": [ 9.10735972051835, 42.151727300558832 ] } }
+]
+}
